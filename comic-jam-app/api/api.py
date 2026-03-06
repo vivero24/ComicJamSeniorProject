6be7f66d51828771b181e965aa6511ea007cc3d9
Ada <ada@fedoravm.notarealaddress>
2026-03-05 23:20:54 -0500

    Created api/ and minimal flask server, added Socket.IO to dependencies, added connection test to homepage

---
+++ comic-jam-app/api/api.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'replace_with_something_secure'
+socketio = SocketIO(app, cors_allowed_origins='*')
+
+connection_count = 0
+
+@socketio.on('connect')
+def connect_handler():
+    print('Connection established')
+    
+    global connection_count
+    connection_count = connection_count + 1
+    emit('user-count-update', connection_count, broadcast=True)
+
+@socketio.on('disconnect')
+def disconnect_handler():
+    print('User disconnected')
+
+    global connection_count
+    connection_count = connection_count - 1
+    emit('user-count-update', connection_count, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
