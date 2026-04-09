c225fb8500aa5a0f0af81430b31785a3d46ca60b
vivero24 <vivero24@students.rowan.edu>
2026-04-09 00:20:01 -0400

    working timer now

--- comic-jam-app/src/components/PlayerGame.jsx
+++ comic-jam-app/src/components/PlayerGame.jsx
@@ -4,12 +4,30 @@ export default function PlayerGame({ onDataSend })
 {
     const[currRound, setCurrRound] = useState(0)
     const[totalRounds, setTotalRound] = useState(0)
-    const[initialTimeLimit, setInitialTimeLimit] = useState(30)
+    const [initialTimeLimit, setInitialTimeLimit] = useState(30)
     const[timeRemaining, setTimeRemaining] = useState(initialTimeLimit)
+
+    //let timeLeft = initialTimeLimit;
+
+    function updateTimer()
+    {
+        //timeLeft --;
+        //prevTime is the current state value at the time
+        setTimeRemaining(prevTime => prevTime - 1);
+        console.log(timeRemaining);
+    }
+
+    useEffect(() =>
+    {
+        //
+        const interval = setInterval(updateTimer, 1000);
+        return () => clearInterval(interval);
+    }, []);
 
 
     return (
         <>
+
             <h1>
                 Player Game Debug
             </h1>
@@ -21,9 +39,11 @@ export default function PlayerGame({ onDataSend })
                 Round {currRound} of {totalRounds}
             </div>
             <div>
+                
                 Time Remaining: {timeRemaining}
             </div>
             <div>
+
                 Players Still drawing:
             </div>
             <div>
